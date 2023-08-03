87403f6f8613785065ce74d41594a826883f947a
Nurullah Kardaş <115217234+nurullhkrds@users.noreply.github.com>
2023-08-03 14:11:48 +0300

    kodluyoruz

---
+++ ReCapProject/ConsoleUII/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using DataAccess.Concrete;
+
+CarManager manager = new CarManager(new InMemoryCarDal());
+foreach (var car in manager.GetAll())
+{
+    Console.WriteLine(car.ModelYear);
+}
